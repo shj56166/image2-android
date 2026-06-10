219f2d296051c43ee9914f56b62b835e447485fd
shj56166 <shj56166@outlook.com>
2026-06-10 19:13:35 +0800

    Prepare v1.0.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.shj56166androidimage2.app"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
